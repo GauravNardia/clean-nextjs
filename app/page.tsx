ca39909508555c43fa9b4a38274ad2dc0557a1d8
Gaurav Nardia <codegaurav07@gmail.com>
2026-05-13 19:29:45 +0530

    changed somethin on branch master

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
        This is clean nextjs app with no dockerfile and no docker compose file and no nextjs config predefined
        <button className="bg-blue-500 text-white p-2 rounded-md">Click me</button>
+       <p className="text-red-500">changing branch named master</p>
     </div>
   );
 }
