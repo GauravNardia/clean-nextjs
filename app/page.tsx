cbdcf1e5380505ca771211fa915fab2ab0ca815f
Gaurav Nardia <codegaurav07@gmail.com>
2026-05-13 19:22:57 +0530

    ad btn

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
        This is clean nextjs app with no dockerfile and no docker compose file and no nextjs config predefined
+       <button className="bg-blue-500 text-white p-2 rounded-md">Click me</button>
     </div>
   );
 }
