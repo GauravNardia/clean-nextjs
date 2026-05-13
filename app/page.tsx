34934b5bf8526e89da34dc701cba67b5310eb936
Gaurav Nardia <codegaurav07@gmail.com>
2026-05-13 20:09:57 +0530

    rollback

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-       This is clean nextjs app with no dockerfile and no docker compose file and no nextjs config predefined
-       <button className="bg-blue-500 text-white p-2 rounded-md">Click me</button>
-       <p className="text-red-500">changing branch named master</p>
+        <h1>Implement the rollbacks some work here</h1>
     </div>
   );
 }
