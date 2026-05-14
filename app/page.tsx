c3a0441752a9e74a02130e1bcee74bfa54991861
Gaurav Nardia <codegaurav07@gmail.com>
2026-05-14 12:22:10 +0530

    auto webhook deployed

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
      <h1>
       THis is the autodeploy webhook testing 2 fixeddddddddddd....
+      this is the new line added by the developer
+      this is the new line added by the developer
+      this is the new line added by the developer
+      this is the new line added by the developer
      </h1>
     </div>
   );
