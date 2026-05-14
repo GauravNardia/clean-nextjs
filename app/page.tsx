ba24e609f04e43f363dbbfefcc7916b63e37cd8b
Gaurav Nardia <codegaurav07@gmail.com>
2026-05-14 09:58:42 +0530

    webhook test 2

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
      <h1>
-      THis is the autodeploy webhook testing....
+      THis is the autodeploy webhook testing 2....
      </h1>
     </div>
   );
