f4387ca3acb86b0296f7335e548dfd1769a27cff
Gaurav Nardia <codegaurav07@gmail.com>
2026-05-14 07:22:18 +0530

    update home

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <h1>Implement the rollbacks some work here</h1>
+        <h1>Implementing the deployement message correction agent</h1>
+        <p>This is a simple agent that will correct the deployment message.</p>
+        <p>The agent will use the following steps:</p>
+        <ol>
+            <li>Read the deployment message</li>
+            <li>Correct the deployment message</li>
+            <li>Return the corrected deployment message</li>
+        </ol>
     </div>
   );
 }
