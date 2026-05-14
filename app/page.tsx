2ff4b3a099d9b88b7c701b8118da96c5f24e7ae5
Gaurav Nardia <codegaurav07@gmail.com>
2026-05-14 09:55:19 +0530

    test webhook

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <h1>Implementing the deployement message correction agent</h1>
-        <p>This is a simple agent that will correct the deployment message.</p>
-        <p>The agent will use the following steps:</p>
-        <ol>
-            <li>Read the deployment message</li>
-            <li>Correct the deployment message</li>
-            <li>Return the corrected deployment message</li>
-        </ol>
+     <h1>
+      THis is the autodeploy webhook testing....
+     </h1>
     </div>
   );
 }
