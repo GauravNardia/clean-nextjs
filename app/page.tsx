bae585aa480dee9a50b3770b8b28fea20fec9546
Gaurav Nardia <codegaurav07@gmail.com>
2026-05-15 13:08:41 +0530

    feat: 60 push

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <h1 className="text-4xl font-bold">Hello World 50 gaurav nardia</h1>
+        <h1 className="text-4xl font-bold">Hello World 60 gaurav nardia</h1>
         <p>Im testing the github autodeploy webhook to better vercel</p>
+        <p>NEXT_PUBLIC_URL: {process.env.NEXT_PUBLIC_URL}</p>
       </div>
     </div>
   );
