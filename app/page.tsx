e405e2d2fa7927fd75e46c26f93b98832a4a3374
Gaurav Nardia <codegaurav07@gmail.com>
2026-05-15 09:38:21 +0530

    feat: new home 7

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <h1 className="text-4xl font-bold">Hello World 6</h1>
+        <h1 className="text-4xl font-bold">Hello World 7</h1>
         <p>Im testing the github autodeploy webhook to better vercel</p>
       </div>
     </div>
