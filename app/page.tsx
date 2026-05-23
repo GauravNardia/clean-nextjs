ad14d78e3e907d7ec0a85064b54ebad25dcc606d
Gaurav Nardia <codegaurav07@gmail.com>
2026-05-23 21:12:28 +0530

    preview

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
         <p>Im testing the github autodeploy webhook to better vercel</p>
         <p>NEXT_PUBLIC_URL: {process.env.NEXT_PUBLIC_URL}</p>
         <h1>This is production/preview testing 8</h1>
+        <p>this is previewwwwwwwwww</p>
       </div>
     </div>
   );
