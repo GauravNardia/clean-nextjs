d3e8a0f4a4c4381754c761d2de4338bc0c331238
Gaurav Nardia <codegaurav07@gmail.com>
2026-05-25 08:47:22 +0530

    pushing to preview

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <h1 className="text-4xl font-bold">Hello World 100 gaurav nardia</h1>
-        <p>Im testing the github autodeploy webhook to better vercel</p>
-        <p>NEXT_PUBLIC_URL: {process.env.NEXT_PUBLIC_URL}</p>
-        <h1>This is production/preview testing 8</h1>
-        <p>this is previewwwwwwwwww navbar branch</p>
-      </div>
-    </div>
+        <h1 className="text-4xl font-bold">This is preview branch</h1>
+        </div>
   );
 }
