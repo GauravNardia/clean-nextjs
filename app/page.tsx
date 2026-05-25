e7bdc4f42c3c7e9e0aa46f26e700158d712b21dd
Gaurav Nardia <codegaurav07@gmail.com>
2026-05-25 13:29:37 +0530

    switch to preview

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>2nd time pushing in preview managed</h1>
-      <h1>3rd time pushing in preview managed</h1>
+      <h1>Switched to  preview branch</h1>
     </div>
   );
 }
