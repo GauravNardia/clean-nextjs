e51e4df1c3b1c4df05cf424afec71728e2a537f4
Gaurav Nardia <codegaurav07@gmail.com>
2026-05-25 08:41:21 +0530

    >2nd time pushing in prod

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <h1 className="text-4xl font-bold">Hello World 100 gaurav nardia</h1>
-        <p>Im testing the github autodeploy webhook to better vercel</p>
-        <p>NEXT_PUBLIC_URL: {process.env.NEXT_PUBLIC_URL}</p>
-        <h1>This is production/preview testing 8</h1>
-<h1>PRODUCTIONNNNNN</h1>
-<p>this is previewwwwwwww navbar branch</p>
-    </div>
+      <h1>2nd time pushing in prod managed</h1>
     </div>
   );
 }
