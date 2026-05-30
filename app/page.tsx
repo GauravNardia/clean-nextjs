5342a666e3a4fd1a7285653eaa03b703f38e88f9
Gaurav Nardia <codegaurav07@gmail.com>
2026-05-30 08:35:41 +0530

    auto deploy testing

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="bg-blue-500 text-white text-2xl">Auto deploy testing updations</h1>
       <h1>2nd time pushing in prod managed</h1>
       <h1>3rd time pushing in prod managed</h1>
       <h1>4th time pushing in prod managed</h1>
